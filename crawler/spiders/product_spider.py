b51d1275b356c3b56e5a716b1c67478fed5efaac
mhb_dev <bayatim81@gmail.com>
2021-11-16 21:31:42 +0330

    Edit product_spider.py added 7 page crawling

--- crawler/spiders/product_spider.py
+++ crawler/spiders/product_spider.py
@@ -3,12 +3,18 @@ import scrapy
 
 class DrinkSpider(scrapy.Spider):
     name = 'drink'
-    start_urls = ['https://okala.com/drinks-herbaltea?pageNumber=7']
+    my_urls = []
+    num_of_pages = 7
+    for i in range(1, num_of_pages+1):
+        my_urls.append('https://okala.com/drinks-herbaltea?pageNumber='+str(i))
+    start_urls = my_urls
 
     def parse(self, response, **kwargs):
         drinks = response.css('div.col-lg-3.col-md-4.col-sm-6.p-1')
         print(len(drinks))
-        # for drink in drinks:
-        #     yield {
-        #
-        #     }
+        for drink in drinks:
+            print(drink.css('h2::name'))
+            # yield {
+            #     'id': drink.css('')
+            #
+            #  }

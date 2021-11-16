0f322a0faad3b0b5a9378b3e54c07f2a30655854
mhb_dev <bayatim81@gmail.com>
2021-11-16 18:15:20 +0330

    Edited product_spider.py Spider can crawl a single page now

--- crawler/spiders/product_spider.py
+++ crawler/spiders/product_spider.py
@@ -2,8 +2,13 @@ import scrapy
 
 
 class DrinkSpider(scrapy.Spider):
-    name = 'drink crawler'
-    start_urls = ['https://okala.com/drinks-herbaltea']
+    name = 'drink'
+    start_urls = ['https://okala.com/drinks-herbaltea?pageNumber=7']
 
     def parse(self, response, **kwargs):
-        pass
+        drinks = response.css('div.col-lg-3.col-md-4.col-sm-6.p-1')
+        print(len(drinks))
+        # for drink in drinks:
+        #     yield {
+        #
+        #     }

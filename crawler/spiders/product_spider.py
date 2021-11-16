56683effdd723c61d76ca98c5eca2991f1b3bfb5
mhb_dev <bayatim81@gmail.com>
2021-11-16 15:42:38 +0330

    Created DrinkSpider class

--- crawler/spiders/product_spider.py
+++ crawler/spiders/product_spider.py
@@ -1,3 +1,9 @@
 import scrapy
 
 
+class DrinkSpider(scrapy.Spider):
+    name = 'drink crawler'
+    start_urls = ['https://okala.com/drinks-herbaltea']
+
+    def parse(self, response, **kwargs):
+        pass

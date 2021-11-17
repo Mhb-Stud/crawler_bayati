8cd068c371dfe9d35e29f6a24d49c7c16307e118
mhb_dev <bayatim81@gmail.com>
2021-11-17 08:26:16 +0330

    Crawls the products of 7 pages then sends a json response

--- crawler/spiders/product_spider.py
+++ crawler/spiders/product_spider.py
@@ -1,20 +1,32 @@
 import scrapy
+import requests
 
 
 class DrinkSpider(scrapy.Spider):
     name = 'drink'
     my_urls = []
-    num_of_pages = 7
+    num_of_pages = 1
     for i in range(1, num_of_pages+1):
         my_urls.append('https://okala.com/drinks-herbaltea?pageNumber='+str(i))
     start_urls = my_urls
+    request_url = 'http://127.0.0.1:8000/'
 
     def parse(self, response, **kwargs):
         drinks = response.css('div.col-lg-3.col-md-4.col-sm-6.p-1')
         print(len(drinks))
         for drink in drinks:
-            print(drink.css('h2::name'))
             # yield {
-            #     'id': drink.css('')
-            #
-            #  }
+            #     "product_id": drink.css('a.product-box_image').attrib['data-productid'],
+            #     "Product_title": drink.css('a.product-box_image').attrib['title'],
+            #     "product_price": drink.css('div.product-box_price-value.text-secondary::text').get(),
+            #     "price_before_discount": drink.css('div.product-box_price-value.text-secondary::text').get(),
+            # }
+            product_id = drink.css('a.product-box_image').attrib['data-productid']
+            product_title = drink.css('a.product-box_image').attrib['title']
+            product_price = (drink.css('div.product-box_price-value.text-secondary::text').get()).replace(' ', '')
+            r = requests.post(self.request_url, json={
+                "product_id": int(product_id),
+                "Product_title": product_title,
+                "product_price": int(product_price.replace(',', '')),
+                "price_before_discount": int(product_price.replace(',', ''))
+            })

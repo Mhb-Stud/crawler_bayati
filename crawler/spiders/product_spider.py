b572bc1d461045959d453614ef798e121d3c3055
mhb_dev <bayatim81@gmail.com>
2021-11-17 21:26:21 +0330

    Changed concurency option in settings.py and Fixed 429 error by implementing a different solution in product_spider.py

--- crawler/spiders/product_spider.py
+++ crawler/spiders/product_spider.py
@@ -1,19 +1,17 @@
 import scrapy
 import requests
+import time
 
 
 class DrinkSpider(scrapy.Spider):
     name = 'drink'
-    my_urls = []
     num_of_pages = 7
-    for i in range(1, num_of_pages+1):
-        my_urls.append('https://okala.com/drinks-herbaltea?pageNumber='+str(i))
-    start_urls = my_urls
+    counter = 2
+    start_urls = ['https://okala.com/drinks-herbaltea']
     request_url = 'http://127.0.0.1:8000/'
 
     def parse(self, response, **kwargs):
         drinks = response.css('div.col-lg-3.col-md-4.col-sm-6.p-1')
-        print(len(drinks))
         for drink in drinks:
             product_id = drink.css('a.product-box_image').attrib['data-productid']
             product_title = drink.css('a.product-box_image').attrib['title']
@@ -24,3 +22,8 @@ class DrinkSpider(scrapy.Spider):
                 "product_price": int(product_price.replace(',', '')),
                 "price_before_discount": int(product_price.replace(',', ''))
             })
+        if DrinkSpider.num_of_pages >= DrinkSpider.counter:
+            clicks = response.css('a.page-link')
+            next_page = "https://okala.com/drinks-herbaltea?pageNumber=" + str(DrinkSpider.counter)
+            DrinkSpider.counter = DrinkSpider.counter + 1
+            yield response.follow(next_page, callback=self.parse)

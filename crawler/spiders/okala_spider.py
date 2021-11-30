dba78a1208206fbf3c136e3c5f1b866356197c7c
mhb_dev <bayatim81@gmail.com>
2021-11-30 13:43:16 +0330

    Changed spider name repositioned Comments Changed request library to scrapy and made special first request

--- crawler/spiders/okala_spider.py
+++ crawler/spiders/okala_spider.py
@@ -1,40 +1,43 @@
 import scrapy
-import requests
-
+from scrapy import Request
+import json
 
 class DrinkSpider(scrapy.Spider):
+
     # name of our spider
-    name = 'drink'
+    name = 'okala'
+
     # how many pages do you want the crawler to get
     num_of_pages = 7
+
     # don't touch
     counter = 2
-    # getting url for first page
+
     start_urls = ['https://okala.com/drinks-herbaltea']
+
     # address of our api app that we want to send data to
     request_url = 'http://127.0.0.1:8000/api/'
 
-    """ 
-    crawls the website first finding products by their css tag then 
-    extracting all their attributes such as product_id,title,...
-    then whe use requests module to sent a post request with the json 
-    data extracted to request_url after we are done we find the next
-    page url and pass the url to response.follow now it calls the
-    parse with next_page url and so on until we hit num of pages
-    """
     def parse(self, response, **kwargs):
+        """ crawls the website first finding products by their css tag then
+        extracting all their attributes such as product_id,title,...
+        then whe use requests module to sent a post request with the json
+        data extracted to request_url after we are done we find the next
+        page url and pass the url to response.follow now it calls the
+        parse with next_page url and so on until we hit num of pages
+        """
         drinks = response.css('div.col-lg-3.col-md-4.col-sm-6.p-1')
         for drink in drinks:
             product_id = drink.css('a.product-box_image').attrib['data-productid']
             product_title = drink.css('a.product-box_image').attrib['title']
             product_price = (drink.css('div.product-box_price-value.text-secondary::text').get()).replace(' ', '')
-            r = requests.post(self.request_url, json={
+            yield Request(self.request_url, body=json.dumps({
                 "id": int(product_id),
                 "title": product_title.replace(' ', '-'),
                 "price": int(product_price.replace(',', '')),
                 "base_price": int(product_price.replace(',', '')),
                 "vendor": "okala"
-            })
+            }), method='POST', headers={'Content-Type': 'application/json'})
         if DrinkSpider.num_of_pages >= DrinkSpider.counter:
             next_page = "https://okala.com/drinks-herbaltea?pageNumber=" + str(DrinkSpider.counter)
             DrinkSpider.counter = DrinkSpider.counter + 1
--- crawler/spiders/tezol_spider.py
+++ crawler/spiders/tezol_spider.py
@@ -1,29 +1,37 @@
-import requests
 import scrapy
+from scrapy import Request
 import json
+from scrapy.http import Response
 
+
+# noinspection PyDictCreation
 class TezolSpider(scrapy.Spider):
     name = 'tezol'
-    start_urls = ['https://www.tezolmarket.com/Home/GetProductQueryResult']
+    # start_urls = ['https://www.tezolmarket.com/Home/GetProductQueryResult']
     request_url = 'https://www.tezolmarket.com/Home/GetProductQueryResult'
     endpoint_url = 'http://127.0.0.1:8000/api/'
 
+    def start_requests(self):
+        yield Request(self.request_url, body=json.dumps({
+            "AppliedAttributeValueIds": None,
+            "SearchTerm": None,
+            "SearchPrice": None,
+            "PageIndex": 0,
+            "CategoryId": 46,
+            "AppliedBrandIds": None
+        }), method='POST', headers={'Content-Type': 'application/json'})
+
     def parse(self, response, **kwargs):
-        api_response = requests.post(self.request_url, json={
-        "AppliedAttributeValueIds": None,
-        "SearchTerm": None,
-        "SearchPrice": None,
-        "PageIndex": 0,
-        "CategoryId": 46,
-        "AppliedBrandIds": None
-        })
-        resp_in_json = api_response.json()
-        deserialized = resp_in_json.get("Products")
-        for product in deserialized:
+        response_in_json = json.loads(response.body)
+        products = response_in_json.get("Products")
+        for product in products:
             temp_dic = {}
             temp_dic['id'] = product['ProductId']
             temp_dic['title'] = product['FullName'].replace(' ', '-')
-            temp_dic['price'] = int(product['FinalUnitPrice'])*10
-            temp_dic['base_price'] = int(product['FinalUnitPrice'])*10
+            temp_dic['price'] = int(product['FinalUnitPrice']) * 10
+            temp_dic['base_price'] = int(product['FinalUnitPrice']) * 10
             temp_dic['vendor'] = "tezol"
-            r = requests.post(self.endpoint_url, json=temp_dic)
+            yield Request(self.endpoint_url, body=json.dumps(temp_dic), method='POST', headers={'Content-Type': 'application/json'})
+
+
+

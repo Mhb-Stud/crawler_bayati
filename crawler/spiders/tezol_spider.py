c78723d91a231563d7200cd7e7f8de4ab0cf3ce1
mhb_dev <bayatim81@gmail.com>
2021-12-02 00:08:10 +0330

    Added data sender method for dry purpose also tested crawl functionality for crawling cattegories succesfuly

--- crawler/spiders/tezol_spider.py
+++ crawler/spiders/tezol_spider.py
@@ -7,47 +7,46 @@ from scrapy.http import Response
 # noinspection PyDictCreation
 class TezolSpider(scrapy.Spider):
     name = 'tezol'
-    # start_urls = ['https://www.tezolmarket.com/Home/GetProductQueryResult']
+    start_urls = ['https://www.tezolmarket.com']
     request_url = 'https://www.tezolmarket.com/Home/GetProductQueryResult'
     endpoint_url = 'http://127.0.0.1:8000/api/'
-    response = None
-
-    def start_requests(self):
-        yield Request(self.request_url, body=json.dumps({
-            "AppliedAttributeValueIds": None,
-            "SearchTerm": None,
-            "SearchPrice": None,
-            "PageIndex": 0,
-            "CategoryId": 46,
-            "AppliedBrandIds": None
-        }), method='POST', headers={'Content-Type': 'application/json'})
+    #
+    # def start_requests(self):
+    #     yield Request(self.request_url, body=json.dumps({
+    #         "AppliedAttributeValueIds": None,
+    #         "SearchTerm": None,
+    #         "SearchPrice": None,
+    #         "PageIndex": 0,
+    #         "CategoryId": 46,
+    #         "AppliedBrandIds": None
+    #     }), method='POST', headers={'Content-Type': 'application/json'})
 
     def parse(self, response, **kwargs):
-        response_in_json = json.loads(response.body)
-        self.product_scraper(response_in_json)
+        # response_in_json = json.loads(response.body)
+        # self.product_scraper(response_in_json)
         Request('https://www.tezolmarket.com')
         categories = response.css('ul.children.col-xs-12.text-right.wrap').css('div.menuColWrap')
         for menuColRap in categories:
             category_name = menuColRap.css('img.menuImg.menuImgHeight').attrib['alt']
-            self.data_sender(None, category_name)
+            yield self.data_sender(None, category_name)
             menu_cols = menuColRap.css('div.menuCol')
             for menuCol in menu_cols:
                 sub_category_name = menuCol.css('h4::text')
                 super_category = category_name
-                self.data_sender(super_category, sub_category_name)
+                yield self.data_sender(super_category, sub_category_name)
                 sub_menus = menuCol.css('ul.subMenu').css('li')
                 for sub_menu in sub_menus:
                     sub_sub_category_name = sub_menu.css('a::text')
                     parent_category = sub_category_name
-                    self.data_sender(parent_category, sub_sub_category_name)
+                    yield self.data_sender(parent_category, sub_sub_category_name)
 
-    def data_sender(self, super_category, category_name):
+    def data_sender(self, super_category, name):
         temp_dic = {
-            'category_name': category_name,
+            'name': name,
             'super_category': super_category
         }
-        yield Request(self.endpoint_url, body=json.dumps(temp_dic), method='POST',
-                      headers={'Content-Type': 'application/json'})
+        return Request(self.endpoint_url, body=json.dumps(temp_dic), method='POST',
+                       headers={'Content-Type': 'application/json'})
 
     def product_scraper(self, response_in_json):
         products = response_in_json.get("Products")

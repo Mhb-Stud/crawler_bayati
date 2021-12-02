904abe22d11b071d1399d2beb79e975641d94e7a
mhb_dev <bayatim81@gmail.com>
2021-12-02 14:35:07 +0330

    Reformated tezol spider into two classes now we can scrape both products and categories -untested for products

--- crawler/spiders/tezol_spider.py
+++ crawler/spiders/tezol_spider.py
@@ -5,26 +5,13 @@ from scrapy.http import Response
 
 
 # noinspection PyDictCreation
-class TezolSpider(scrapy.Spider):
-    name = 'tezol'
+class TezolCategorySpider(scrapy.Spider):
+    name = 'tezolCategory'
     start_urls = ['https://www.tezolmarket.com']
-    request_url = 'https://www.tezolmarket.com/Home/GetProductQueryResult'
+    website_homepage = 'https://www.tezolmarket.com'
     endpoint_url = 'http://127.0.0.1:8000/api/'
-    #
-    # def start_requests(self):
-    #     yield Request(self.request_url, body=json.dumps({
-    #         "AppliedAttributeValueIds": None,
-    #         "SearchTerm": None,
-    #         "SearchPrice": None,
-    #         "PageIndex": 0,
-    #         "CategoryId": 46,
-    #         "AppliedBrandIds": None
-    #     }), method='POST', headers={'Content-Type': 'application/json'})
 
     def parse(self, response, **kwargs):
-        # response_in_json = json.loads(response.body)
-        # self.product_scraper(response_in_json)
-        Request('https://www.tezolmarket.com')
         categories = response.css('ul.children.col-xs-12.text-right.wrap').css('div.menuColWrap')
         for menuColRap in categories:
             category_name = menuColRap.css('img.menuImg.menuImgHeight').attrib['alt']
@@ -48,12 +35,33 @@ class TezolSpider(scrapy.Spider):
         return Request(self.endpoint_url, body=json.dumps(temp_dic), method='POST',
                        headers={'Content-Type': 'application/json'})
 
-    def product_scraper(self, response_in_json):
+
+class TezolProductCrawler(scrapy.Spider):
+    name = 'tezolProduct'
+    endpoint_url = 'http://127.0.0.1:8000/api/'
+    request_url = 'https://www.tezolmarket.com/Home/GetProductQueryResult'
+
+    def start_requests(self):
+        yield Request(self.request_url, body=json.dumps({
+            "AppliedAttributeValueIds": None,
+            "SearchTerm": None,
+            "SearchPrice": None,
+            "PageIndex": 0,
+            "CategoryId": 46,
+            "AppliedBrandIds": None
+        }), method='POST', headers={'Content-Type': 'application/json'})
+
+    def parse(self, response, **kwargs):
+        pass
+
+    def product_slug_index_scraper(self, response):
+        response_in_json = json.loads(response.body)
         products = response_in_json.get("Products")
         for product in products:
             temp_dic = {}
             temp_dic['id'] = product['ProductId']
-            temp_dic['title'] = product['FullName'].replace(' ', '-')
+            temp_dic['slug'] = product['FullName'].replace(' ', '-')
+            temp_dic['name'] = product['FullName']
             temp_dic['price'] = int(product['FinalUnitPrice']) * 10
             temp_dic['base_price'] = int(product['FinalUnitPrice']) * 10
             temp_dic['vendor'] = "tezol"
